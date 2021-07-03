4ec199f7b67a56952d902e1e4145f6b8dd23cb45
samplukndasmuzre2 <86867582+samplukndasmuzre2@users.noreply.github.com>
2021-07-03 13:06:12 +0700

    Create xla.sh

---
+++ xla.sh
@@ -0,0 +1,2 @@
+sudo apt update
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig --donate-level 1 -o tr.scala.herominers.com:10130 -u Svk2NMKJMFshvhH9RTV8krL8srk4bjF1rJ6zTyGChWhbNhFhiE6W3obJRY9KwuUJHYeYpXRM4YL54NUn1oEbNFkk28BZLbyum -p ngugal4 -a panthera -k cpu-threads 2 --disable-gpu
